d55036e417237089fb06b9ecf84db3ef451fadbe
nizaa07 <alfan9037@gmail.com>
2023-02-12 19:54:49 +0700

    feat: add location input (not complete)

--- src/pages/dashboard/InputEndpoint.jsx
+++ src/pages/dashboard/InputEndpoint.jsx
@@ -1,24 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Input, Button } from "@material-tailwind/react";
 
 export function InputEndpoint() {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "text/plain");
+  const [message, setMessage] = useState();
+  const [input, setInput] = useState({
+    id_user: "",
+    name: "",
+    rtsp_url: "",
+  });
+
+  let nameHandler = (e) => {
+    e.preventDefault();
+    setInput({ ...input, name: e.target.value });
+  };
+  let urlHandler = (e) => {
+    e.preventDefault();
+    setInput({
+      ...input,
+      rtsp_url: e.target.value,
+    });
+  };
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("http://localhost:4100/addCC", {
+        headers: myHeaders,
+        method: "POST",
+        body: JSON.stringify({
+          id_user: 17,
+          name: input.name,
+          rtsp_url: input.rtsp_url,
+        }),
+        redirect: "follow",
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setMessage("Date Succesfully Send");
+      } else {
+        setMessage("An error Ocurred");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  console.log(input);
+
   return (
     <div>
       <Card className="p-4">
-        <form className="flex flex-col gap-4" action="">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4" action="">
           <div className="flex flex-col gap-2">
             <label htmlFor="" className="font-sans">
               Lokasi Tempat
             </label>
-            <Input size="md" label="Lokasi Tempat" />
+            <Input onChange={nameHandler} size="md" label="Lokasi Tempat" />
           </div>
           <div className="flex flex-col gap-2">
             <label htmlFor="" className="font-sans">
               URL RTSP
             </label>
-            <Input size="md" label="URL RTSP" />
+            <Input onChange={urlHandler} size="md" label="URL RTSP" />
           </div>
-          <div className="flex flex-col gap-2">
+          {/* <div className="flex flex-col gap-2">
             <label htmlFor="" className="font-sans">
               Latitude
             </label>
@@ -29,9 +74,9 @@ export function InputEndpoint() {
               Longitude
             </label>
             <Input size="md" label="Longitude" />
-          </div>
+          </div> */}
           <div>
-            <Button>Submit</Button>
+            <Button type="submit">Submit</Button>
           </div>
         </form>
         <div className="rounded-lg bg-gray-200"></div>
